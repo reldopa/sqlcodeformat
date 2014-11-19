edc2e9495b591e404d58cbfcbc44830412b12b27
thiago.boca@gmail.com <thiago.boca@gmail.com@3a3006b7-fd13-c837-6c1b-5a2cda08bdcd>
2014-11-19 10:35:12 +0000

    Correções e atualizações

--- sqlcodeformatproject --username thiago.boca@gmail.com/SQLCodeFormat/SQLCodeFormat/FormParametros.Designer.cs
+++ sqlcodeformatproject --username thiago.boca@gmail.com/SQLCodeFormat/SQLCodeFormat/FormParametros.Designer.cs
@@ -64,7 +64,7 @@
             this.groupBox5.Size = new System.Drawing.Size(482, 435);
             this.groupBox5.TabIndex = 2;
             this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Formatting Options";
+            this.groupBox5.Text = "Opções de Formatação";
             this.groupBox5.Enter += new System.EventHandler(this.groupBox5_Enter);
             // 
             // tableLayoutPanel2
@@ -114,7 +114,6 @@
             this.grp_Options.Size = new System.Drawing.Size(470, 382);
             this.grp_Options.TabIndex = 4;
             this.grp_Options.TabStop = false;
-            this.grp_Options.Text = "Standard Formatting Options";
             // 
             // chk_BreakJoinOnSections
             // 
@@ -122,9 +121,9 @@
             this.chk_BreakJoinOnSections.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_BreakJoinOnSections.Location = new System.Drawing.Point(8, 281);
             this.chk_BreakJoinOnSections.Name = "chk_BreakJoinOnSections";
-            this.chk_BreakJoinOnSections.Size = new System.Drawing.Size(139, 17);
+            this.chk_BreakJoinOnSections.Size = new System.Drawing.Size(199, 17);
             this.chk_BreakJoinOnSections.TabIndex = 18;
-            this.chk_BreakJoinOnSections.Text = "Break Join ON Sections";
+            this.chk_BreakJoinOnSections.Text = "Quebrar cláusula JOIN na seção ON";
             this.chk_BreakJoinOnSections.UseVisualStyleBackColor = true;
             // 
             // chk_ExpandInLists
@@ -135,9 +134,9 @@
             this.chk_ExpandInLists.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_ExpandInLists.Location = new System.Drawing.Point(8, 258);
             this.chk_ExpandInLists.Name = "chk_ExpandInLists";
-            this.chk_ExpandInLists.Size = new System.Drawing.Size(100, 17);
+            this.chk_ExpandInLists.Size = new System.Drawing.Size(130, 17);
             this.chk_ExpandInLists.TabIndex = 17;
-            this.chk_ExpandInLists.Text = "Expand IN Lists";
+            this.chk_ExpandInLists.Text = "Expandir listas com IN";
             this.chk_ExpandInLists.UseVisualStyleBackColor = true;
             // 
             // chk_EnableKeywordStandardization
@@ -146,14 +145,14 @@
             this.chk_EnableKeywordStandardization.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_EnableKeywordStandardization.Location = new System.Drawing.Point(8, 350);
             this.chk_EnableKeywordStandardization.Name = "chk_EnableKeywordStandardization";
-            this.chk_EnableKeywordStandardization.Size = new System.Drawing.Size(179, 17);
+            this.chk_EnableKeywordStandardization.Size = new System.Drawing.Size(222, 17);
             this.chk_EnableKeywordStandardization.TabIndex = 17;
-            this.chk_EnableKeywordStandardization.Text = "Enable Keyword Standardization";
+            this.chk_EnableKeywordStandardization.Text = "Habilitar padronização de palavras-chave";
             this.chk_EnableKeywordStandardization.UseVisualStyleBackColor = true;
             // 
             // txt_ClauseBreaks
             // 
-            this.txt_ClauseBreaks.Location = new System.Drawing.Point(116, 95);
+            this.txt_ClauseBreaks.Location = new System.Drawing.Point(134, 95);
             this.txt_ClauseBreaks.Name = "txt_ClauseBreaks";
             this.txt_ClauseBreaks.Size = new System.Drawing.Size(38, 20);
             this.txt_ClauseBreaks.TabIndex = 16;
@@ -165,13 +164,13 @@
             this.lbl_ClauseBreaks.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.lbl_ClauseBreaks.Location = new System.Drawing.Point(7, 98);
             this.lbl_ClauseBreaks.Name = "lbl_ClauseBreaks";
-            this.lbl_ClauseBreaks.Size = new System.Drawing.Size(78, 13);
+            this.lbl_ClauseBreaks.Size = new System.Drawing.Size(107, 13);
             this.lbl_ClauseBreaks.TabIndex = 15;
-            this.lbl_ClauseBreaks.Text = "Clause Breaks:";
+            this.lbl_ClauseBreaks.Text = "Quebra de cláusulas:";
             // 
             // txt_StatementBreaks
             // 
-            this.txt_StatementBreaks.Location = new System.Drawing.Point(116, 71);
+            this.txt_StatementBreaks.Location = new System.Drawing.Point(134, 71);
             this.txt_StatementBreaks.Name = "txt_StatementBreaks";
             this.txt_StatementBreaks.Size = new System.Drawing.Size(38, 20);
             this.txt_StatementBreaks.TabIndex = 16;
@@ -183,13 +182,13 @@
             this.lbl_StatementBreaks.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.lbl_StatementBreaks.Location = new System.Drawing.Point(7, 73);
             this.lbl_StatementBreaks.Name = "lbl_StatementBreaks";
-            this.lbl_StatementBreaks.Size = new System.Drawing.Size(94, 13);
+            this.lbl_StatementBreaks.Size = new System.Drawing.Size(121, 13);
             this.lbl_StatementBreaks.TabIndex = 15;
-            this.lbl_StatementBreaks.Text = "Statement Breaks:";
+            this.lbl_StatementBreaks.Text = "Quebra de declarações:";
             // 
             // txt_MaxWidth
             // 
-            this.txt_MaxWidth.Location = new System.Drawing.Point(77, 46);
+            this.txt_MaxWidth.Location = new System.Drawing.Point(86, 45);
             this.txt_MaxWidth.Name = "txt_MaxWidth";
             this.txt_MaxWidth.Size = new System.Drawing.Size(77, 20);
             this.txt_MaxWidth.TabIndex = 16;
@@ -201,9 +200,9 @@
             this.lbl_MaxWidth.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.lbl_MaxWidth.Location = new System.Drawing.Point(7, 49);
             this.lbl_MaxWidth.Name = "lbl_MaxWidth";
-            this.lbl_MaxWidth.Size = new System.Drawing.Size(61, 13);
+            this.lbl_MaxWidth.Size = new System.Drawing.Size(77, 13);
             this.lbl_MaxWidth.TabIndex = 15;
-            this.lbl_MaxWidth.Text = "Max Width:";
+            this.lbl_MaxWidth.Text = "Largura lateral:";
             // 
             // txt_IndentWidth
             // 
@@ -219,9 +218,9 @@
             this.lbl_IndentWidth.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.lbl_IndentWidth.Location = new System.Drawing.Point(9, 23);
             this.lbl_IndentWidth.Name = "lbl_IndentWidth";
-            this.lbl_IndentWidth.Size = new System.Drawing.Size(71, 13);
+            this.lbl_IndentWidth.Size = new System.Drawing.Size(58, 13);
             this.lbl_IndentWidth.TabIndex = 13;
-            this.lbl_IndentWidth.Text = "Indent Width:";
+            this.lbl_IndentWidth.Text = "Identação:";
             // 
             // chk_ExpandBetweenConditions
             // 
@@ -231,9 +230,9 @@
             this.chk_ExpandBetweenConditions.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_ExpandBetweenConditions.Location = new System.Drawing.Point(9, 234);
             this.chk_ExpandBetweenConditions.Name = "chk_ExpandBetweenConditions";
-            this.chk_ExpandBetweenConditions.Size = new System.Drawing.Size(171, 17);
+            this.chk_ExpandBetweenConditions.Size = new System.Drawing.Size(199, 17);
             this.chk_ExpandBetweenConditions.TabIndex = 10;
-            this.chk_ExpandBetweenConditions.Text = "Expand BETWEEN Conditions";
+            this.chk_ExpandBetweenConditions.Text = "Expandir condições com BETWEEN";
             this.chk_ExpandBetweenConditions.UseVisualStyleBackColor = true;
             // 
             // chk_SpaceAfterComma
@@ -242,9 +241,9 @@
             this.chk_SpaceAfterComma.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_SpaceAfterComma.Location = new System.Drawing.Point(42, 165);
             this.chk_SpaceAfterComma.Name = "chk_SpaceAfterComma";
-            this.chk_SpaceAfterComma.Size = new System.Drawing.Size(120, 17);
+            this.chk_SpaceAfterComma.Size = new System.Drawing.Size(186, 17);
             this.chk_SpaceAfterComma.TabIndex = 9;
-            this.chk_SpaceAfterComma.Text = "Space After Comma";
+            this.chk_SpaceAfterComma.Text = "Espaço em branco após a vírgula";
             this.chk_SpaceAfterComma.UseVisualStyleBackColor = true;
             // 
             // chk_Coloring
@@ -253,9 +252,9 @@
             this.chk_Coloring.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_Coloring.Location = new System.Drawing.Point(8, 327);
             this.chk_Coloring.Name = "chk_Coloring";
-            this.chk_Coloring.Size = new System.Drawing.Size(100, 17);
+            this.chk_Coloring.Size = new System.Drawing.Size(93, 17);
             this.chk_Coloring.TabIndex = 8;
-            this.chk_Coloring.Text = "Enable Coloring";
+            this.chk_Coloring.Text = "Habilitar cores";
             this.chk_Coloring.UseVisualStyleBackColor = true;
             // 
             // chk_UppercaseKeywords
@@ -266,9 +265,9 @@
             this.chk_UppercaseKeywords.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_UppercaseKeywords.Location = new System.Drawing.Point(8, 304);
             this.chk_UppercaseKeywords.Name = "chk_UppercaseKeywords";
-            this.chk_UppercaseKeywords.Size = new System.Drawing.Size(127, 17);
+            this.chk_UppercaseKeywords.Size = new System.Drawing.Size(167, 17);
             this.chk_UppercaseKeywords.TabIndex = 7;
-            this.chk_UppercaseKeywords.Text = "Uppercase Keywords";
+            this.chk_UppercaseKeywords.Text = "Palavras-chave em maiúsculo";
             this.chk_UppercaseKeywords.UseVisualStyleBackColor = true;
             // 
             // chk_ExpandCaseStatements
@@ -279,9 +278,9 @@
             this.chk_ExpandCaseStatements.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_ExpandCaseStatements.Location = new System.Drawing.Point(9, 211);
             this.chk_ExpandCaseStatements.Name = "chk_ExpandCaseStatements";
-            this.chk_ExpandCaseStatements.Size = new System.Drawing.Size(149, 17);
+            this.chk_ExpandCaseStatements.Size = new System.Drawing.Size(182, 17);
             this.chk_ExpandCaseStatements.TabIndex = 6;
-            this.chk_ExpandCaseStatements.Text = "Expand CASE Statements";
+            this.chk_ExpandCaseStatements.Text = "Expandir declarações com CASE";
             this.chk_ExpandCaseStatements.UseVisualStyleBackColor = true;
             // 
             // chk_ExpandBooleanExpressions
@@ -292,9 +291,9 @@
             this.chk_ExpandBooleanExpressions.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_ExpandBooleanExpressions.Location = new System.Drawing.Point(9, 188);
             this.chk_ExpandBooleanExpressions.Name = "chk_ExpandBooleanExpressions";
-            this.chk_ExpandBooleanExpressions.Size = new System.Drawing.Size(163, 17);
+            this.chk_ExpandBooleanExpressions.Size = new System.Drawing.Size(175, 17);
             this.chk_ExpandBooleanExpressions.TabIndex = 5;
-            this.chk_ExpandBooleanExpressions.Text = "Expand Boolean Expressions";
+            this.chk_ExpandBooleanExpressions.Text = "Expandir expressões booleanas";
             this.chk_ExpandBooleanExpressions.UseVisualStyleBackColor = true;
             // 
             // chk_TrailingCommas
@@ -303,9 +302,9 @@
             this.chk_TrailingCommas.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_TrailingCommas.Location = new System.Drawing.Point(42, 142);
             this.chk_TrailingCommas.Name = "chk_TrailingCommas";
-            this.chk_TrailingCommas.Size = new System.Drawing.Size(103, 17);
+            this.chk_TrailingCommas.Size = new System.Drawing.Size(102, 17);
             this.chk_TrailingCommas.TabIndex = 4;
-            this.chk_TrailingCommas.Text = "Trailing Commas";
+            this.chk_TrailingCommas.Text = "Vírgulas no final";
             this.chk_TrailingCommas.UseVisualStyleBackColor = true;
             // 
             // chk_ExpandCommaLists
@@ -316,9 +315,9 @@
             this.chk_ExpandCommaLists.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.chk_ExpandCommaLists.Location = new System.Drawing.Point(9, 119);
             this.chk_ExpandCommaLists.Name = "chk_ExpandCommaLists";
-            this.chk_ExpandCommaLists.Size = new System.Drawing.Size(124, 17);
+            this.chk_ExpandCommaLists.Size = new System.Drawing.Size(178, 17);
             this.chk_ExpandCommaLists.TabIndex = 3;
-            this.chk_ExpandCommaLists.Text = "Expand Comma Lists";
+            this.chk_ExpandCommaLists.Text = "Expandir declaração de campos";
             this.chk_ExpandCommaLists.UseVisualStyleBackColor = true;
             // 
             // FormParametros
